d1b0ce485c4ff5201a4cdfaa1a3c8b23f42de9cc
LaaSofiaa <lab@labinfo.is.escuelaing.edu.co>
2024-08-22 15:59:47 -0500

    ShapeMain class

--- src/main/java/edu/eci/cvds/patterns/archetype/App.java
+++ src/main/java/edu/eci/cvds/patterns/archetype/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( String.join(" ",args) );
+        System.out.println("Hello" + String.join(" ",args) );
     }
 }
